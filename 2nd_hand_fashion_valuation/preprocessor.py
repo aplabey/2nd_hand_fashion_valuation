d4e2bedaee968831ea4567b04b876bf812aa47b9
simoneliesnig <simoneliesnig@gmail.com>
2024-09-03 17:03:58 +0200

    filled what is already clear

---
+++ 2nd_hand_fashion_valuation/preprocessor.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+from colorama import Fore, Style
+
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer
